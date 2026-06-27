ab64713c7a8f380015cfb11d71fcadac0b43049b
loctran312 <lommlay@gmail.com>
2026-06-27 09:50:15 +0700

    Update CORS add method PATCH

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ const app = express();
 app.use(cors({
     origin: process.env.FRONTEND_URL || '*',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 }));
 
