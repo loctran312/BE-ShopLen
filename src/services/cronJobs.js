183abfb386c58f59ca227a4c62e849aad69cb387
loctran312 <lommlay@gmail.com>
2026-07-11 11:52:31 +0700

    Update cron job

--- src/services/cronJobs.js
+++ src/services/cronJobs.js
@@ -2,12 +2,12 @@ const pool = require('../config/db');
 const cron = require('node-cron');
 
 const initCronJobs = () => {
-    cron.schedule('*/30 * * * *', async () => {
+    cron.schedule('*/3 * * * *', async () => {
         await pool.query(`
             UPDATE don_hang 
             SET trang_thai = 'cancelled' 
             WHERE trang_thai = 'pending' 
-              AND ngay_tao < NOW() - INTERVAL '15 minutes'
+              AND ngay_tao < NOW() - INTERVAL '3 minutes'
               AND don_hang_id IN (SELECT don_hang_id FROM thanh_toan WHERE phuong_thuc = 'MOMO')
         `);
         console.log('Cron Job: Đã dọn dẹp các đơn MoMo chưa thanh toán.');
